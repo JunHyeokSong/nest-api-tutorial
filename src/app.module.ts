e0a4d478f03831d84e310f77a6ce1cf76ee14290
Song <shinobu2624@unist.ac.kr>
2022-02-24 23:57:35 +0900

    add config module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,18 @@ import { AuthModule } from './auth/auth.module';
 import { UserModule } from './user/user.module';
 import { BookmarkModule } from './bookmark/bookmark.module';
 import { ModelModule } from './model/model.module';
+import { ConfigModule } from '@nestjs/config';
 
 
 @Module({
-  imports: [AuthModule, UserModule, BookmarkModule, ModelModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    AuthModule,
+    UserModule, 
+    BookmarkModule, 
+    ModelModule
+  ],
 })
-export class AppModule {}
+export class AppModule { }
